6ccd5c094a61105df76e3481a3bfe223763246f5
Aniketgiram <aniketgiram2404@gmail.com>
2021-03-28 07:14:20 +0530

    Init Commit

---
+++ app/src/main/java/com/example/liquorpurchase/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.liquorpurchase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webview = (WebView) findViewById(R.id.webview);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("http://demo.helixstack.in/");
+
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webview.canGoBack()){
+            webview.goBack();
+        }else {
+            super.onBackPressed();
+        }
+    }
+}
